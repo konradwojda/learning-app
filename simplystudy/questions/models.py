1b7cf394be808762c4eb3f98b03773d6c2e49065
Konrad Wojda <konrad2001@outlook.com>
2023-05-18 17:25:53 +0900

    Added initial models

--- simplystudy/questions/models.py
+++ simplystudy/questions/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    content = models.TextField(max_length=10000)
+    answer = models.TextField(max_length=10000)
+    image = models.ImageField()
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+
+class QuestionSet(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(null=True, blank=True, max_length=10000)
+    course_name = models.CharField(max_length=512)
+    questions = models.ManyToManyField(Question)
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
