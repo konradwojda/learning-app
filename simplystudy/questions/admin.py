1278e98b790bf521adca1f90471b01351529ce9f
Konrad Wojda <konrad2001@outlook.com>
2023-05-18 16:28:21 +0900

    Typo fix

--- simplystudy/questions/admin.py
+++ simplystudy/questions/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from models import Question, QuestionSet
+
+from .models import Question, QuestionSet
 
 admin.site.register(QuestionSet)
 admin.site.register(Question)
