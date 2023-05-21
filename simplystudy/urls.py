d5fe39b500eb86127e93a0ea8aebee7838d47d5e
Konrad Wojda <konrad2001@outlook.com>
2023-05-21 16:38:02 +0900

    Added rest_framework to url routing

--- simplystudy/urls.py
+++ simplystudy/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from simplystudy.questions import views
+
+router = routers.DefaultRouter()
+router.register(r"questions", views.QuestionViewSet)
+router.register(r"question_sets", views.QuestionSetViewSet)
 
 urlpatterns = [
+    path("", include(router.urls)),
     path("admin/", admin.site.urls),
 ]
