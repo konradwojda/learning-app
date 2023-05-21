c2ba4bd961b28449563fcd615bc5d7510a7f4a09
Konrad Wojda <konrad2001@outlook.com>
2023-05-21 16:37:36 +0900

    Created views for question and questionset models

--- simplystudy/questions/views.py
+++ simplystudy/questions/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from simplystudy.questions.models import Question, QuestionSet
+from simplystudy.questions.serializers import QuestionSerializer, QuestionSetSerializer
+
+
+class QuestionViewSet(viewsets.ModelViewSet): # pylint: disable=too-many-ancestors
+    """ViewSet dla modelu Question"""
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class QuestionSetViewSet(viewsets.ModelViewSet): # pylint: disable=too-many-ancestors
+    """ViewSet dla modelu QuestionSet"""
+    queryset = QuestionSet.objects.all()
+    serializer_class = QuestionSetSerializer
+    permission_classes = [permissions.IsAuthenticated]
