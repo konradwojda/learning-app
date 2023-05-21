b162c11f41d0908d5e7b9baa72c1a835b9bee8f3
Konrad Wojda <konrad2001@outlook.com>
2023-05-21 16:39:48 +0900

    Formatting fix

--- simplystudy/questions/serializers.py
+++ simplystudy/questions/serializers.py
@@ -5,12 +5,15 @@ from simplystudy.questions.models import Question, QuestionSet
 
 class QuestionSerializer(serializers.ModelSerializer):
     """Serializer dla modelu Question"""
+
     class Meta:
         model = Question
         fields = "__all__"
 
+
 class QuestionSetSerializer(serializers.ModelSerializer):
     """Serializer dla modelu QuestionSet"""
+
     class Meta:
         model = QuestionSet
         fields = "__all__"
--- simplystudy/questions/views.py
+++ simplystudy/questions/views.py
@@ -4,14 +4,17 @@ from simplystudy.questions.models import Question, QuestionSet
 from simplystudy.questions.serializers import QuestionSerializer, QuestionSetSerializer
 
 
-class QuestionViewSet(viewsets.ModelViewSet): # pylint: disable=too-many-ancestors
+class QuestionViewSet(viewsets.ModelViewSet):  # pylint: disable=too-many-ancestors
     """ViewSet dla modelu Question"""
+
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-class QuestionSetViewSet(viewsets.ModelViewSet): # pylint: disable=too-many-ancestors
+
+class QuestionSetViewSet(viewsets.ModelViewSet):  # pylint: disable=too-many-ancestors
     """ViewSet dla modelu QuestionSet"""
+
     queryset = QuestionSet.objects.all()
     serializer_class = QuestionSetSerializer
     permission_classes = [permissions.IsAuthenticated]
