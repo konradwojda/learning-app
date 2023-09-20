7c54f7051db690ad58d048bc109867b45f59cd8c
Konrad Wojda <konrad2001@outlook.com>
2023-09-21 00:12:02 +0200

    Changes to models

--- simplystudy/questions/models.py
+++ simplystudy/questions/models.py
@@ -1,17 +1,54 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-class Question(models.Model):
-    content = models.TextField(max_length=10000)
-    answer = models.TextField(max_length=10000)
-    image = models.ImageField()
-    owner = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
+class Course(models.Model):
+    """Klasa reprezentująca kurs, do którego mogą być przypisywane zestawy pytań"""
+
+    name = models.CharField(max_length=512)
+    university = models.CharField(max_length=512)
+    description = models.TextField(null=True, blank=True)
 
 
 class QuestionSet(models.Model):
-    name = models.CharField(max_length=256)
-    description = models.TextField(null=True, blank=True, max_length=10000)
-    course_name = models.CharField(max_length=512)
-    questions = models.ManyToManyField(Question)
+    """Klasa reprezentująca zestaw pytań do nauki"""
+
+    name = models.CharField(max_length=512)
+    description = models.TextField(null=True, blank=True)
+    course = models.ForeignKey(to=Course, null=True, blank=True, on_delete=models.SET_NULL)
     owner = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
+
+
+class Question(models.Model):
+    """Klasa reprezentująca pojedyńcze pytanie (fiszkę)"""
+
+    content = models.TextField()
+    answer = models.TextField()
+    image = models.ImageField(blank=True, null=True)
+    question_set = models.ForeignKey(
+        to=QuestionSet, related_name="questions", on_delete=models.CASCADE
+    )
+
+
+class Test(models.Model):
+    """Klasa reprezentująca test dla danego zestawu pytań"""
+
+    name = models.CharField(max_length=512)
+    question_set = models.ForeignKey(
+        to=QuestionSet, related_name="tests", on_delete=models.CASCADE
+    )
+
+
+class TestQuestion(models.Model):
+    """Klasa reprezentująca pytanie testowe"""
+
+    class TestQuestionType(models.TextChoices):
+        """Klasa reprezentująca typ pytania testowego"""
+
+        TEXT_ANSWER = "TEXT", _("Text answer")
+        MULTIPLE_CHOICE = "MULTIPLE", _("Multiple choice")
+        SINGLE_CHOICE = "SINGLE", _("Single choice")
+
+    test = models.ForeignKey(to=Test, related_name="test_questions", on_delete=models.CASCADE)
+    question_type = models.CharField(max_length=128, choices=TestQuestionType.choices)
