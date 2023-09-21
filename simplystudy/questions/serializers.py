e77f179df23b97ec0e261f63a4b085eb26497ad9
Konrad Wojda <konrad2001@outlook.com>
2023-09-21 21:12:01 +0200

    Added serializers for new models

--- simplystudy/questions/serializers.py
+++ simplystudy/questions/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 
-from simplystudy.questions.models import Question, QuestionSet
+from simplystudy.questions.models import Course, Question, QuestionSet, Test, TestQuestion
+
+
+class CourseSerializer(serializers.ModelField):
+    """Serializer dla Course"""
+
+    class Meta:
+        model = Course
+        fields = "__all__"
 
 
 class QuestionSerializer(serializers.ModelSerializer):
@@ -16,4 +24,20 @@ class QuestionSetSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = QuestionSet
+        fields = "__all__"
+
+
+class TestSerializer(serializers.ModelSerializer):
+    """Serializer dla Test"""
+
+    class Meta:
+        model = Test
+        fields = "__all__"
+
+
+class TestQuestionSerializer(serializers.ModelSerializer):
+    """Serializer dla TestQuestion"""
+
+    class Meta:
+        model = TestQuestion
         fields = "__all__"
