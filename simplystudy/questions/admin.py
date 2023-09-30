bf8bdf8bea0f9f98fd83e9b14f7d04d991731703
Konrad Wojda <konrad2001@outlook.com>
2023-09-30 14:31:38 +0200

    Added new models to admin site

--- simplystudy/questions/admin.py
+++ simplystudy/questions/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Question, QuestionSet
+from .models import Course, Question, QuestionSet, Test, TestQuestion
 
 admin.site.register(QuestionSet)
 admin.site.register(Question)
+admin.site.register(Course)
+admin.site.register(Test)
+admin.site.register(TestQuestion)
