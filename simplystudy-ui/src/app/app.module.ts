3abee6e6e360f1be86318cfc4475987791e5ddc5
Konrad Wojda <konrad2001@outlook.com>
2023-10-01 17:16:05 +0200

    Added auth service and auth-login component

--- simplystudy-ui/src/app/app.module.ts
+++ simplystudy-ui/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
+import { AuthLoginComponent } from './auth-login/auth-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AuthLoginComponent,
   ],
   imports: [
     BrowserModule,
