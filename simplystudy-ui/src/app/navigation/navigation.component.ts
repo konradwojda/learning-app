c0908fcfb5a1a1e8894f40c73cab56fb4fb54c72
Konrad Wojda <konrad2001@outlook.com>
2023-10-01 13:31:12 +0200

    Added side navigation

--- simplystudy-ui/src/app/navigation/navigation.component.ts
+++ simplystudy-ui/src/app/navigation/navigation.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css'],
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule],
   standalone: true,
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
 
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addEventListener('change', this._mobileQueryListener);
+  };
+  ngOnDestroy(): void {
+    this.mobileQuery.removeEventListener('change', this._mobileQueryListener)
+  }
 }
