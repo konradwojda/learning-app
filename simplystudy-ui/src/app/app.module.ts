73835541066fec861537224e6749b9e743f3cede
Konrad Wojda <konrad2001@outlook.com>
2023-10-05 16:48:07 +0200

    Fixed unused import

--- simplystudy-ui/src/app/app.module.ts
+++ simplystudy-ui/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthLoginComponent } from './auth-login/auth-login.component';
-import { HTTP_INTERCEPTORS, HttpClientModule, HttpClientXsrfModule, HttpXsrfTokenExtractor } from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule, HttpClientXsrfModule } from "@angular/common/http";
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
 import { ReactiveFormsModule } from "@angular/forms";
