3b907d1f021564ccd788185dfe5338b66a72e0a3
Konrad Wojda <konrad2001@outlook.com>
2023-10-10 21:25:59 +0200

    First version of dashboard

--- simplystudy-ui/src/app/dashboard/dashboard.component.ts
+++ simplystudy-ui/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { QuestionSet } from '../question-sets/question-set';
+import { AuthService } from '../auth.service';
+import { environment } from 'src/environments/environment';
+import { SnackbarService } from '../snackbar.service';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { NgFor } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
   standalone: true,
+  imports: [MatGridListModule, NgFor, MatCardModule]
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  questionSets: Array<QuestionSet> = [];
+  private apiUrl = environment.apiUrl;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient, private authService: AuthService, private snackbarService: SnackbarService) {
+  }
+
+  ngOnInit(): void {
+    let username = this.authService.getUsername();
+    this.http.get<Array<QuestionSet>>(this.apiUrl + '/api/question_sets/?username=' + username).subscribe({
+      next: (data: Array<QuestionSet>) => this.questionSets = data,
+      error: (error) => {
+        this.snackbarService.showSnackbar(error.error.detail);
+      }
+    });
+  };
 }
