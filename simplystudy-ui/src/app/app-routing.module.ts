9233a9ffa94a74d042f2066d7e0c9832b3fa03bb
Konrad Wojda <konrad2001@outlook.com>
2023-10-12 20:49:19 +0200

    Added new component for creating question set

--- simplystudy-ui/src/app/app-routing.module.ts
+++ simplystudy-ui/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { AuthRegisterComponent } from './auth-register/auth-register.component';
 import { QuestionSetsComponent } from './question-sets/question-sets.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateQuestionSetComponent } from './create-question-set/create-question-set.component';
 
 const routes: Routes = [
     { path: '', component: LandingPageComponent },
     { path: 'login', component: AuthLoginComponent },
     { path: 'register', component: AuthRegisterComponent },
     { path: 'dashboard', component: DashboardComponent },
-    { path: 'question_sets/:id', component: QuestionSetsComponent }
+    { path: 'question_sets/:id', component: QuestionSetsComponent },
+    { path: 'question_sets/new', component: CreateQuestionSetComponent }
 ];
 
 @NgModule({
---
+++ simplystudy-ui/src/app/create-question-set/create-question-set.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-create-question-set',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './create-question-set.component.html',
+  styleUrls: ['./create-question-set.component.css']
+})
+export class CreateQuestionSetComponent {
+
+}
