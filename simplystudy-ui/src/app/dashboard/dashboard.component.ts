9c8ccfd7459479b76d7bdfd6383ddf43f7bfa264
Konrad Wojda <konrad2001@outlook.com>
2023-10-12 16:31:35 +0200

    Added dashboard component to see user resources

--- simplystudy-ui/src/app/dashboard/dashboard.component.ts
+++ simplystudy-ui/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { QuestionSet } from '../question-sets/question-set';
 import { AuthService } from '../auth.service';
 import { environment } from 'src/environments/environment';
@@ -8,20 +8,25 @@ import { SnackbarService } from '../snackbar.service';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { NgFor } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatRippleModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
   standalone: true,
-  imports: [MatGridListModule, NgFor, MatCardModule]
+  imports: [MatGridListModule, NgFor, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatListModule, MatRippleModule]
 })
 export class DashboardComponent implements OnInit {
 
   questionSets: Array<QuestionSet> = [];
   private apiUrl = environment.apiUrl;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient, private authService: AuthService, private snackbarService: SnackbarService) {
+  constructor(private router: Router, private http: HttpClient, private authService: AuthService, private snackbarService: SnackbarService) {
   }
 
   ngOnInit(): void {
@@ -33,4 +38,8 @@ export class DashboardComponent implements OnInit {
       }
     });
   };
+
+  public onCardClick(event: any) {
+    this.router.navigateByUrl('/question_sets/' + event.id);
+  }
 }
