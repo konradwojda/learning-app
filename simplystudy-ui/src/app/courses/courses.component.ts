c3dd4c58d63214c7cc76a14d40f2863682aa8762
Konrad Wojda <konrad2001@outlook.com>
2023-10-13 19:45:40 +0200

    Added example courses view.

--- simplystudy-ui/src/app/courses/courses.component.ts
+++ simplystudy-ui/src/app/courses/courses.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Course } from './course';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { AuthService } from '../auth.service';
+import { SnackbarService } from '../snackbar.service';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
+  imports: [CommonModule, MatListModule],
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [];
+  private apiUrl = environment.apiUrl;
 
+  constructor(private http: HttpClient, private authService: AuthService, private snackbarService: SnackbarService) {
+  }
+
+  ngOnInit(): void {
+    let username = this.authService.getUsername();
+    this.http.get<Course[]>(this.apiUrl + '/api/courses/?username=' + username).subscribe({
+      next: (data: Course[]) => {
+        this.courses = data;
+      },
+      error: (error) => {
+        this.snackbarService.showSnackbar(error.error.detail);
+      }
+    })
+  }
 }
