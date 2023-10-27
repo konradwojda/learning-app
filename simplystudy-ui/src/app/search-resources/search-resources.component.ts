a3db6a7dcf60df69e0605a04b14262e00d5effd4
Konrad Wojda <konrad2001@outlook.com>
2023-10-27 13:43:30 +0200

    Added list of sets in serach resources

--- simplystudy-ui/src/app/search-resources/search-resources.component.ts
+++ simplystudy-ui/src/app/search-resources/search-resources.component.ts
@@ -3,21 +3,24 @@ import { Component, OnInit } from '@angular/core';
 import { QuestionSet } from '../question-sets/question-set';
 import { environment } from 'src/environments/environment';
 import { SnackbarService } from '../snackbar.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
+import { MatRippleModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-search-resources',
   templateUrl: './search-resources.component.html',
   styleUrls: ['./search-resources.component.css'],
   standalone: true,
-  imports: [NgFor]
+  imports: [NgFor, MatCardModule, MatRippleModule, NgIf]
 })
 export class SearchResourcesComponent implements OnInit {
 
   resources: Array<QuestionSet> = [];
   private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient, private snackbarService: SnackbarService) {
+  constructor(private http: HttpClient, private snackbarService: SnackbarService, private router: Router) {
 
   }
 
@@ -30,5 +33,9 @@ export class SearchResourcesComponent implements OnInit {
         this.snackbarService.showSnackbar(error.error.detail);
       }
     })
+  }
+
+  public onCardClick(event: any) {
+    this.router.navigateByUrl('/question_sets/' + event.id);
   }
 }
