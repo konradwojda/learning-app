8600439794cb0ceeea96ab7897135458f5e199d7
Konrad Wojda <konrad2001@outlook.com>
2023-10-31 15:37:29 +0100

    Added searching

--- simplystudy-ui/src/app/search-resources/search-resources.component.ts
+++ simplystudy-ui/src/app/search-resources/search-resources.component.ts
@@ -8,13 +8,16 @@ import { NgFor, NgIf } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { Router } from '@angular/router';
 import { MatRippleModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-search-resources',
   templateUrl: './search-resources.component.html',
   styleUrls: ['./search-resources.component.css'],
   standalone: true,
-  imports: [NgFor, MatCardModule, MatRippleModule, NgIf, MatPaginatorModule]
+  imports: [NgFor, MatCardModule, MatRippleModule, NgIf, MatPaginatorModule, MatInputModule, FormsModule, MatButtonModule]
 })
 export class SearchResourcesComponent implements OnInit {
 
@@ -26,13 +29,16 @@ export class SearchResourcesComponent implements OnInit {
   pageIndex: number = 0;
   pageSizeOptions: number[] = [10, 20, 50, 75];
 
+  searchText: string = '';
+  searchUrl: string = ''
+
 
   constructor(private http: HttpClient, private snackbarService: SnackbarService, private router: Router) {
 
   }
 
-  ngOnInit(): void {
-    this.http.get(this.apiUrl + '/api/public_question_sets/?page_size=' + this.pageSize).subscribe({
+  getResources(): void {
+    this.http.get(this.apiUrl + '/api/public_question_sets/?page_size=' + this.pageSize + this.searchUrl).subscribe({
       next: (data: any) => {
         this.items_count = data.count
         this.resources = data.results;
@@ -43,11 +49,15 @@ export class SearchResourcesComponent implements OnInit {
     })
   }
 
+  ngOnInit(): void {
+    this.getResources();
+  }
+
   handlePageEvent(e: PageEvent) {
     this.pageSize = e.pageSize;
     this.pageIndex = e.pageIndex;
     let query_idx = e.pageIndex + 1;
-    this.http.get(this.apiUrl + '/api/public_question_sets/?page=' + query_idx + '&' + 'page_size=' + this.pageSize).subscribe({
+    this.http.get(this.apiUrl + '/api/public_question_sets/?page=' + query_idx + '&' + 'page_size=' + this.pageSize + this.searchUrl).subscribe({
       next: (data: any) => {
         this.resources = data.results;
       }
@@ -56,5 +66,15 @@ export class SearchResourcesComponent implements OnInit {
 
   public onCardClick(event: any) {
     this.router.navigateByUrl('/question_sets/' + event.id);
+  }
+
+  searchResources(): void {
+    if (this.searchText) {
+      this.searchUrl = '&search=' + this.searchText;
+      this.getResources();
+    }
+    else {
+      this.searchUrl = '';
+    }
   }
 }
