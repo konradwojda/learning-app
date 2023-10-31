7e1ec0e385c8dae24d2d9fe133667d467b8b8cee
Konrad Wojda <konrad2001@outlook.com>
2023-10-31 14:58:56 +0100

    Added pagination to search resources

--- simplystudy-ui/src/app/search-resources/search-resources.component.ts
+++ simplystudy-ui/src/app/search-resources/search-resources.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { QuestionSet } from '../question-sets/question-set';
 import { environment } from 'src/environments/environment';
 import { SnackbarService } from '../snackbar.service';
+import { PageEvent, MatPaginatorModule } from '@angular/material/paginator';
 import { NgFor, NgIf } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { Router } from '@angular/router';
@@ -13,24 +14,42 @@ import { MatRippleModule } from '@angular/material/core';
   templateUrl: './search-resources.component.html',
   styleUrls: ['./search-resources.component.css'],
   standalone: true,
-  imports: [NgFor, MatCardModule, MatRippleModule, NgIf]
+  imports: [NgFor, MatCardModule, MatRippleModule, NgIf, MatPaginatorModule]
 })
 export class SearchResourcesComponent implements OnInit {
 
   resources: Array<QuestionSet> = [];
   private apiUrl = environment.apiUrl;
 
+  items_count: number = 0;
+  pageSize: number = 10;
+  pageIndex: number = 0;
+  pageSizeOptions: number[] = [10, 20, 50, 75];
+
+
   constructor(private http: HttpClient, private snackbarService: SnackbarService, private router: Router) {
 
   }
 
   ngOnInit(): void {
-    this.http.get<Array<QuestionSet>>(this.apiUrl + '/api/question_sets/').subscribe({
-      next: (data: Array<QuestionSet>) => {
-        this.resources = data;
+    this.http.get(this.apiUrl + '/api/public_question_sets/?page_size=' + this.pageSize).subscribe({
+      next: (data: any) => {
+        this.items_count = data.count
+        this.resources = data.results;
       },
       error: (error) => {
         this.snackbarService.showError(error);
+      }
+    })
+  }
+
+  handlePageEvent(e: PageEvent) {
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
+    let query_idx = e.pageIndex + 1;
+    this.http.get(this.apiUrl + '/api/public_question_sets/?page=' + query_idx + '&' + 'page_size=' + this.pageSize).subscribe({
+      next: (data: any) => {
+        this.resources = data.results;
       }
     })
   }
