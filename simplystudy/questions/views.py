2e29fe05e5cb131e6046e02578ff9a771342d0fc
Konrad Wojda <konrad2001@outlook.com>
2023-10-31 15:39:30 +0100

    Isort fix

--- simplystudy/questions/views.py
+++ simplystudy/questions/views.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from django.db.models import Q
 from django.db.models.query import QuerySet
-from rest_framework import permissions, viewsets, filters
+from rest_framework import filters, permissions, viewsets
 from rest_framework.pagination import PageNumberPagination
 
 from simplystudy.questions.models import Course, Question, QuestionSet, Test, TestQuestion
