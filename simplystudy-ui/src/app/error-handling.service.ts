7be5a1d865823b3647d2d3441eb117599d2ede1a
Konrad Wojda <konrad2001@outlook.com>
2023-11-01 17:33:01 +0100

    Added error handling service

---
+++ simplystudy-ui/src/app/error-handling.service.ts
@@ -0,0 +1,23 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { SnackbarService } from './snackbar.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorHandlingService {
+
+  constructor(private router: Router, private snackbarService: SnackbarService) { }
+
+  public handleError(error: HttpErrorResponse): void {
+    if (error.status === 404) {
+      this.snackbarService.showError(error);
+      this.router.navigateByUrl("/404");
+    }
+    else {
+      this.snackbarService.showError(error);
+      this.router.navigateByUrl("/404");
+    }
+  }
+}
