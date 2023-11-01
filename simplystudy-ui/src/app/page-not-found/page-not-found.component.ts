544f2cf36aacb3661a3595f30db1303a000861e9
Konrad Wojda <konrad2001@outlook.com>
2023-11-01 18:10:21 +0100

    Updated 404 page

--- simplystudy-ui/src/app/page-not-found/page-not-found.component.ts
+++ simplystudy-ui/src/app/page-not-found/page-not-found.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.css'],
-  standalone: true
+  standalone: true,
+  imports: [MatButtonModule, RouterModule]
 })
 export class PageNotFoundComponent {
 
