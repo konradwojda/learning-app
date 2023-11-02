ab36bfde0088a3f8e86291cc5f1f82a582004b83
Konrad Wojda <konrad2001@outlook.com>
2023-11-02 14:53:34 +0100

    Added getting user question sets and resources

--- simplystudy-ui/src/app/my-resources/my-resources.component.ts
+++ simplystudy-ui/src/app/my-resources/my-resources.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { UserResource } from '../search-resources/user-resource';
+import { environment } from 'src/environments/environment';
+import { AuthService } from '../auth.service';
+import { ErrorHandlingService } from '../error-handling.service';
+import { QuestionSet } from '../question-sets/question-set';
 
 @Component({
   selector: 'app-my-resources',
@@ -8,6 +15,41 @@ import { CommonModule } from '@angular/common';
   templateUrl: './my-resources.component.html',
   styleUrls: ['./my-resources.component.css']
 })
-export class MyResourcesComponent {
+export class MyResourcesComponent implements OnInit {
+
+  resources: UserResource[] = [];
+  question_sets: QuestionSet[] = [];
+  private apiUrl = environment.apiUrl;
+
+  constructor(private router: Router, private http: HttpClient, private authService: AuthService, private errorHandling: ErrorHandlingService) { }
+
+  private getUserResources(): void {
+    let username = this.authService.getUsername();
+    this.http.get<UserResource[]>(this.apiUrl + '/api/user_resources/?username=' + username).subscribe({
+      next: (data: UserResource[]) => {
+        this.resources = data;
+      },
+      error: (error) => {
+        this.errorHandling.handleError(error);
+      }
+    })
+  }
+
+  private getUserQuestionSets(): void {
+    let username = this.authService.getUsername();
+    this.http.get<Array<QuestionSet>>(this.apiUrl + '/api/question_sets/?username=' + username).subscribe({
+      next: (data: Array<QuestionSet>) => {
+        this.question_sets = data;
+      },
+      error: (error) => {
+        this.errorHandling.handleError(error);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.getUserResources();
+    this.getUserQuestionSets();
+  }
 
 }
