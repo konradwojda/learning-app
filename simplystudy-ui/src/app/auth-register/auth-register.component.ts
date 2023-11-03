521fa1e631ef49845c4263a0e7a4546a5412b901
Konrad Wojda <konrad2001@outlook.com>
2023-11-03 21:58:50 +0100

    Translate in register component

--- simplystudy-ui/src/app/auth-register/auth-register.component.ts
+++ simplystudy-ui/src/app/auth-register/auth-register.component.ts
@@ -9,13 +9,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { SnackbarService } from '../snackbar.service';
 import { Router } from '@angular/router';
 import { ErrorHandlingService } from '../error-handling.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-auth-register',
   templateUrl: './auth-register.component.html',
   styleUrls: ['./auth-register.component.css'],
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, ReactiveFormsModule]
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, ReactiveFormsModule, TranslateModule]
 })
 export class AuthRegisterComponent implements OnInit {
   registerForm: FormGroup;
