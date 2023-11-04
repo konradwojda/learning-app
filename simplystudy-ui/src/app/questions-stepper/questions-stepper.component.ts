eee10d97fe99683b8cbe7b49a6ee87d116ad326f
Konrad Wojda <konrad2001@outlook.com>
2023-11-04 18:36:22 +0100

    Translated stepper

--- simplystudy-ui/src/app/questions-stepper/questions-stepper.component.ts
+++ simplystudy-ui/src/app/questions-stepper/questions-stepper.component.ts
@@ -3,6 +3,7 @@ import { CdkStepper, CdkStepperModule } from '@angular/cdk/stepper';
 import { NgTemplateOutlet, NgFor } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-questions-stepper',
@@ -10,7 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrls: ['./questions-stepper.component.css'],
   providers: [{ provide: CdkStepper, useExisting: QuestionsStepperComponent }],
   standalone: true,
-  imports: [NgTemplateOutlet, CdkStepperModule, NgFor, MatIconModule, MatButtonModule],
+  imports: [NgTemplateOutlet, CdkStepperModule, NgFor, MatIconModule, MatButtonModule, TranslateModule],
 })
 export class QuestionsStepperComponent extends CdkStepper {
 }
