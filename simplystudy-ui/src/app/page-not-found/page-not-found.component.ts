7198b21804f0e8cf674da3e7776ee85cf7d713d2
Konrad Wojda <konrad2001@outlook.com>
2023-11-04 18:34:45 +0100

    Translated 404 page

--- simplystudy-ui/src/app/page-not-found/page-not-found.component.ts
+++ simplystudy-ui/src/app/page-not-found/page-not-found.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.css'],
   standalone: true,
-  imports: [MatButtonModule, RouterModule]
+  imports: [MatButtonModule, RouterModule, TranslateModule]
 })
 export class PageNotFoundComponent {
 
