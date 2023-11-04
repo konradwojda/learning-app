b90f83f5e8d8d8998ec3a7822d15b5f8eb033bea
Konrad Wojda <konrad2001@outlook.com>
2023-11-04 18:13:50 +0100

    Translated landing page

--- simplystudy-ui/src/app/landing-page/landing-page.component.ts
+++ simplystudy-ui/src/app/landing-page/landing-page.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
   standalone: true,
-  imports: [MatButtonModule, RouterModule]
+  imports: [MatButtonModule, RouterModule, TranslateModule]
 })
 export class LandingPageComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) { }
