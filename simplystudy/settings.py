e133983766d465cfa23e039e09dce46f072f6b48
Konrad Wojda <konrad2001@outlook.com>
2023-11-06 19:52:17 +0100

    Loading .env file

--- simplystudy/settings.py
+++ simplystudy/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 APPS_DIR = BASE_DIR / "simplystudy/"
@@ -21,6 +23,7 @@ UI_PATH = "simplystudy-ui/dist/simplystudy-ui"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
