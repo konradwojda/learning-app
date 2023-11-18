7e5176877eefa845932415a2e8c6d01fc73d215e
Konrad Wojda <konrad2001@outlook.com>
2023-11-18 15:17:59 +0100

    Better error handling when 500 returned

--- simplystudy-ui/src/app/error-handling.service.ts
+++ simplystudy-ui/src/app/error-handling.service.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { SnackbarService } from './snackbar.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root',
@@ -10,12 +11,15 @@ export class ErrorHandlingService {
   constructor(
     private router: Router,
     private snackbarService: SnackbarService,
+    private translate: TranslateService
   ) {}
 
   public handleError(error: HttpErrorResponse): void {
     if (error.status === 404) {
       this.snackbarService.showError(error);
       this.router.navigateByUrl('/404');
+    } else if (error.status === 500) {
+      this.snackbarService.showSnackbar(this.translate.instant("Snackbar.ServerError"));
     } else {
       this.snackbarService.showError(error);
     }
