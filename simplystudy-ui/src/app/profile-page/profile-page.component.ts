bfdbe11904ecc77ee1c93e8f9119ba6610abef61
Konrad Wojda <konrad2001@outlook.com>
2023-11-19 12:47:48 +0100

    Created profile page component

---
+++ simplystudy-ui/src/app/profile-page/profile-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-profile-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './profile-page.component.html',
+  styleUrls: ['./profile-page.component.css']
+})
+export class ProfilePageComponent {
+
+}
