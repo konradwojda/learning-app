a118d1e6f6d367d1107423b7ab3a4a0cdf1ed729
Konrad Wojda <konrad2001@outlook.com>
2023-11-23 13:58:01 +0100

    Migration to postgres

--- simplystudy/settings.py
+++ simplystudy/settings.py
@@ -106,8 +106,12 @@ AUTH_USER_MODEL = "users.User"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
