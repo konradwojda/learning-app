7c5fc0e66764857dc92d38645c627c5f5848b1b9
Konrad Wojda <konrad2001@outlook.com>
2023-11-27 19:53:23 +0100

    Tutorial component update

--- simplystudy-ui/src/app/tutorial/tutorial.component.ts
+++ simplystudy-ui/src/app/tutorial/tutorial.component.ts
@@ -1,12 +1,21 @@
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatStepperModule } from '@angular/material/stepper';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-tutorial',
   standalone: true,
-  imports: [MatStepperModule],
+  imports: [MatStepperModule, MatIconModule, TranslateModule],
   templateUrl: './tutorial.component.html',
-  styleUrls: ['./tutorial.component.css']
+  styleUrls: ['./tutorial.component.css'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: {displayDefaultIndicatorType: false},
+    },
+  ],
 })
 export class TutorialComponent {
 
