85cee378a9575d0d270f9d9ff1249815662a375a
Konrad Wojda <konrad2001@outlook.com>
2023-12-07 12:19:59 +0100

    Refactor csv service

--- simplystudy-ui/src/app/services/csv.service.ts
+++ simplystudy-ui/src/app/services/csv.service.ts
@@ -9,31 +9,25 @@ export class CsvService {
 
     if (lines.length < 2) {
       throw new Error(
-        'CSV data is not valid. It should have at least a header and one data row.',
+        'File is invalid. It should contain at least 2 lines. First line should be the header.',
       );
     }
 
     const headers = lines[0].split(',');
     const data: any[] = [];
 
-    for (let i = 1; i < lines.length; i++) {
-      const values = lines[i].split(',');
+    for (const line of lines) {
+      const values = line.split(',');
       const object: any = {};
 
       if (values.length !== headers.length) {
-        throw new Error(
-          `CSV data is not valid on line ${
-            i + 1
-          }. The number of columns doesn't match the header.`,
-        );
+        throw new Error('File is invalid on line: ' + line);
       }
 
       for (let j = 0; j < headers.length; j++) {
         let value: any = values[j].trim();
 
-        if (value === '') {
-          value = null;
-        }
+        value = value === '' ? (value = null) : value;
 
         object[headers[j].trim()] = value;
       }
